c247a4c6f2f1050dfa2d316e0f700f7fc29eec93
lezarius <remy.mazabraud@gmail.com>
2018-03-28 16:59:41 +0200

    change url in footer

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
 
             <ul class="breadcrumb">
               <li class="pull-right"><a href="#top">Back to top</a></li>
-              <li><a href="http://blog.lezarius.eu">Blog</a></li>
+              <li><a href="https://lezarius.science">Blog</a></li>
               <li><a href="https://twitter.com/lezarius">Twitter</a></li>
               <li><a href="https://github.com/lezarius">GitHub</a></li>
-              <li><a href="http://remy-mazabraud.eu">Pro</a></li>
+              <li><a href="http://lezarius.tech">Pro</a></li>
             </ul>
-            <p>Créé par<a href="http://lezarius.eu"> Lezarius</a>.</p>
+            <p>Créé par<a href="http://lezarius.tech"> Lezarius</a>.</p>
           </div>
 
       </footer>
